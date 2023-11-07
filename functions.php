afaa4b27bf49b9b6eb1ac6a66f2b89630e27aeee
imshahastam <shahzada.stamova@gmail.com>
2023-11-07 23:16:43 +0600

    del some comms

--- functions.php
+++ functions.php
@@ -8,11 +8,9 @@ function get_faculties($conn) {
 }
 
 function get_lessons_by_faculty($conn, $faculty_name) {
-    //print_r($faculty_name);
     $lessons_sql = "SELECT l.name FROM lessons l INNER JOIN faculties f ON
     (l.faculty_id = f.faculty_id) WHERE f.name = '$faculty_name'";
     $result = $conn->query($lessons_sql);
-    //print_r($result);
 
     return $result;
 
@@ -20,4 +18,3 @@ function get_lessons_by_faculty($conn, $faculty_name) {
     // var_dump($result);
     // echo '</pre>';
 }
-//get_lessons_by_faculty($conn, 'UME');
