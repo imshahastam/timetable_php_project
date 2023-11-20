854701198457884b347973ca4d38b39c5b6ff073
imshahastam <shahzada.stamova@gmail.com>
2023-11-20 11:11:26 +0600

    add_to_calendar ver 1

--- add_to_calendar.php
+++ add_to_calendar.php
@@ -1,21 +1,29 @@
 <?php
     include("db_connection.php");
-    if(isset($_POST['submit_all'])) {
+
         $subject = $_POST["subject"];
         $teacher = $_POST["teacher"];
         $day = $_POST["day"];
         $time = $_POST["time"];
         $lesson_id = null;
 
-        $lesson_id = mysqli_query($conn, "SELECT lesson_id FROM lessons WHERE name = '$subject'");
+        //$lesson_id_query = mysqli_query($conn, "SELECT lesson_id FROM lessons WHERE name = '$subject'");
+        $lesson_id_query = "SELECT lesson_id FROM lessons WHERE name = '$subject'";
+        $result = $conn->query($lesson_id_query);
+        if($result) {
+            //$row = mysqli_fetch_assoc($lesson_id_query);
+            $row = $result->fetch_assoc();
+            $lesson_id = $row['lesson_id'];
 
-        $query = mysqli_query($conn, "UPDATE lessons SET teacher = '$teacher', times = '$time', days = '$day' 
-        WHERE lesson_id = $lesson_id");
+            $query = mysqli_query($conn, "UPDATE lessons SET teacher = '$teacher', times = '$time', days = '$day' 
+            WHERE lesson_id = $lesson_id");
 
-        if($query) {
-            echo "<script>alert('done')</script>";
+            if($query) {
+                echo "<script>alert('done')</script>";
+            } else {
+                echo "Ошибка выполнения запроса: " . mysqli_error($conn);
+            }
         } else {
-            echo "<script>alert('error')</script>";
+            echo "Ошибка выполнения запроса: " . mysqli_error($conn);
         }
-    }
 ?>
